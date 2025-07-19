b87d0249ab9acd12d04ef188b33baf9ac0847642
anas amellahe <amellaheanas@gmail.com>
2025-07-19 18:37:31 +0100

    feat: initialize new React app with Vite
    - Added package.json with dependencies and scripts for development and build.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Developed main App component with a welcome message, header, user profile, main content, and footer.
    - Added SVG assets for React and Vite logos.
    - Implemented Footer, Header, MainContent, UserProfile, and WelcomeMessage components.
    - Styled components with inline styles and CSS.
    - Set up index.css for global styles.
    - Configured main entry point for the React application.
    - Created Vite configuration file for project setup.

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent()
+{
+
+    return (
+        <main style={{border:'1px solid, black', backgroundColor:'red'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+
+export default MainContent
