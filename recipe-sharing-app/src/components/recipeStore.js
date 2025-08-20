c65ab82cc4e062aff402eddcf33a467de4bd0b29
anas amellahe <amellaheanas@gmail.com>
2025-08-20 22:02:09 +0100

    fix: update import path for useRecipeStore in AddRecipeForm component

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,1 +1,8 @@
-export { useRecipeStore } from '../store/recipeStore';
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
